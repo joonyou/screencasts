49f295685fad49f80e0a85d4a0c25d9915da3e98
Joon You <joon@joonworld.com>
2013-09-02 20:02:56 -0400

    final for rchange

---
+++ rspec_change/rchange_final.rb
@@ -0,0 +1,34 @@
+class Foo
+  def repeat
+    2.times { repeated }
+  end
+
+  def repeated; end
+end
+
+describe Foo do
+  let(:foo) { double method1:"hello" }
+
+  context "stub" do
+    it "is deprecated to use double" do
+      expect(foo.method1).to eq "hello"
+    end
+  end
+
+  context "stub!" do
+    let(:foo) { Foo.new }
+
+    it "is deprecated" do
+      foo.stub bar:"bar"
+      expect(foo.bar).to eq "bar"
+    end
+  end
+
+  context "any_number_of_times" do
+    it "is deprecated" do
+      Foo.any_instance.should_receive(:repeated).at_least(:once)
+      Foo.new.repeat
+    end
+  end
+end
+
